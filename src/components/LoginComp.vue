77f412979bbf9ebb219fb80c3dac9a9f812c9a58
Nguyễn Đức <ducnc@DESKTOP-806L6GP.localdomain>
2023-10-01 23:14:36 +0700

    base

--- src/components/LoginComp.vue
+++ src/components/LoginComp.vue
@@ -10,17 +10,53 @@
     class="btn btn-secondary"
     data-bs-toggle="tooltip"
     data-bs-placement="top"
-    data-bs-custom-class="custom-tooltip"
-    data-bs-title="This top tooltip is themed via CSS variables."
+    data-bs-title="Tooltip on top"
   >
-    Custom tooltip
+    Tooltip on top
   </button>
+  <!-- Button trigger modal -->
+  <button
+    type="button"
+    class="btn btn-primary"
+    data-bs-toggle="modal"
+    data-bs-target="#exampleModal"
+  >
+    Launch demo modal
+  </button>
+
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">...</div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import axios from 'axios'
-import {Tooltip} 'bootstrap'
+import { Tooltip } from 'bootstrap'
 
 const users = ref({
   email: null,
